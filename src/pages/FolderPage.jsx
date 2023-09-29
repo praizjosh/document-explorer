0f57e6e1120b8ad1ac1049c245be78e004f8dabb
Josh <40269551+praizjosh@users.noreply.github.com>
2023-09-29 20:05:14 +0100

    improved grid sizes for screens

--- src/pages/FolderPage.jsx
+++ src/pages/FolderPage.jsx
@@ -70,7 +70,7 @@ const FolderPage = () => {
                     <div className='my-12'>
                         <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md:gap-4">
                             {sortedAndFilteredFiles.map((file) => (
-                                <li key={file.id} className='flex flex-col h-full justify-center items-center bg-slate-200 p-6 rounded'>
+                                <li key={file.id} className='flex flex-col h-full justify-center items-center bg-neutral-200 p-6 rounded'>
                                     <FileItem file={file} />
 
                                 </li>
